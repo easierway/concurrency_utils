94d7122d0ab94514b8d9a370f8b87edeb92d8dab
chaocai <chaocai@192.168.3.44>
2023-01-13 21:07:25 +0800

    added comments

--- asyc_call.go
+++ asyc_call.go
@@ -9,17 +9,22 @@ import (
 var ErrTimeout = errors.New("time out")
 var ErrCancelled = errors.New("task has been cancelled")
 
+// TaskResult is the result of the asynchronous task
 type TaskResult struct {
 	Result interface{}
 	Err    error
 }
 
+// ResultStub is returned immediately after calling AsynExecutor
+// It is the stub to get the result.
 type ResultStub struct {
 	retCh     chan TaskResult
 	timeoutMs int64
 	ctx       context.Context
 }
 
+// GetResult is to get the asynchronous task's result
+// it will be blocked util the task is completed/failed.
 func (rs *ResultStub) GetResult() TaskResult {
 	var tResult TaskResult
 	timer := time.NewTimer(time.Millisecond * time.Duration(rs.timeoutMs))
@@ -39,15 +44,15 @@ func (rs *ResultStub) GetResult() TaskResult {
 
 type Task func(ctx context.Context) TaskResult
 
-// AsynExecutor is to run your text in asynchronous way,
+// AsynExecutor is to run your task in asynchronous way,
 // task is the task need to be executed
 // timeoutMs is the timeout setting for the task execution
 // ResultStub ResultStub.GetResult() is to get the result of the task.
 func AsynExecutor(ctx context.Context, task Task,
-	timeoutMs int64) *ResultStub {
+	timeoutMs int64) ResultStub {
 	retCh := make(chan TaskResult, 1)
 	go func() {
 		retCh <- task(ctx)
 	}()
-	return &ResultStub{retCh, timeoutMs, ctx}
+	return ResultStub{retCh, timeoutMs, ctx}
 }
